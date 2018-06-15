ac17e77e8bead69a1b9283496d25e7b2c7956ea5
Crixus <dagdavid24@yahoo.com>
2018-06-15 18:58:27 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,47 +11,81 @@ var crystalRed = Math.floor(Math.random() * 12) + 1;
 //console.log(crystalRed);
 var red = document.getElementById("red");
 red.setAttribute("value", crystalRed);
+rvalue = parseInt(red.value);
 console.log(red.value);
 
 var crystalBlue = Math.floor(Math.random() * 12) + 1;
 //console.log(crystalBlue);
 var blue = document.getElementById("blue");
 blue.setAttribute("value", crystalBlue);
+bvalue =  parseInt(blue.value);
 console.log(blue.value);
 
 var crystalYellow = Math.floor(Math.random() * 12) + 1;
 //console.log(crystalYellow);
 var yellow = document.getElementById("yellow");
 yellow.setAttribute("value", crystalYellow);
+yValue = parseInt(yellow.value);
 console.log(yellow.value);
 
 var crystalGreen = Math.floor(Math.random() * 12) + 1;
 //console.log(crystalGreen);
 var green = document.getElementById("green");
 green.setAttribute("value", crystalGreen);
+
 console.log(green.value);
 
 
 // add up the value of the number that was assigned to the crystal to yourScore
+var yourScore = document.getElementById("yourScore");
 
 function scoreRed() {
-    var yourScore = document.getElementById("yourScore");
-    yourScore.textContent = red.value;
+    if (yourScore.value > 0) { 
+        yourScore.textContent = parseInt(yourScore.value) + rvalue;
+        yourScore.value = parseInt(yourScore.value) + rvalue;
+    }
+    else {
+        yourScore.textContent = rvalue;
+        yourScore.value = rvalue;
+    }
+    console.log(red.value);
 }
 function scoreBlue() {
-    var yourScore = document.getElementById("yourScore");
-    yourScore.textContent = blue.value;
+    if (yourScore.value > 0) {
+        yourScore.textContent = parseInt(yourScore.value) + bvalue;
+        yourScore.value = parseInt(yourScore.value) + bvalue;
+    }
+    else {
+        yourScore.textContent = bvalue;
+        yourScore = blue.value;
+    }
+    console.log(yourScore);
 }
-function scoreYellow() {
-    var yourScore = document.getElementById("yourScore");
-    yourScore.textContent = yellow.value;
+/*function scoreYellow() {
+    if (yourScore > 0) {
+        yourScore.textContent = parseInt(yourScore.value) + parseInt(yellow.value);
+        yourScore.value = parseInt(yourScore.value) + parseInt(yellow.value);
+    }
+    else {
+        yourScore.textContent = parseInt(yellow.value);
+        yourScore = yellow.value;
+    }
+    console.log(yourScore);
 }
 function scoreGreen() {
-    var yourScore = document.getElementById("yourScore");
-    yourScore.textContent = green.value;
-}
+    if (yourScore > 0) {
+        yourScore.textContent = parseInt(yourScore.value) + parseInt(green.value);
+        yourScore.value = parseInt(yourScore.value) + parseInt(green.value);
+    }
+    else {
+        yourScore.textContent = parseInt(green.value);
+        yourScore = green.value;
+    }
+    //console.log(yourScore);
+}*/
 
 
 // if yourScore is greater than targetScore increment losses and restart game
+
 
 // if yourScore is equall to targetScore increment wins and restart game
