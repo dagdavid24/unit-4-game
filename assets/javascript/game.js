69ac265211a0f2f312a0c2515fc083a0e9cfd9a6
Crixus <dagdavid24@yahoo.com>
2018-07-06 12:15:08 -0400

    Updated javascript to function properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,92 +1,139 @@
 // randomly create a number between 19 - 129 and assign to targetScore
-
-var randomNum = Math.floor(Math.random() * 111) + 19;
-//console.log(randomNum);
-var tar = document.getElementById("targetScore");
-tar.textContent = randomNum;
-
-// assign random value between 1 - 12 for each crystal
-
-var crystalRed = Math.floor(Math.random() * 12) + 1;
-//console.log(crystalRed);
-var red = document.getElementById("red");
-red.setAttribute("value", crystalRed);
-rvalue = parseInt(red.value);
-console.log(red.value);
-
-var crystalBlue = Math.floor(Math.random() * 12) + 1;
-//console.log(crystalBlue);
-var blue = document.getElementById("blue");
-blue.setAttribute("value", crystalBlue);
-bvalue =  parseInt(blue.value);
-console.log(blue.value);
-
-var crystalYellow = Math.floor(Math.random() * 12) + 1;
-//console.log(crystalYellow);
-var yellow = document.getElementById("yellow");
-yellow.setAttribute("value", crystalYellow);
-yValue = parseInt(yellow.value);
-console.log(yellow.value);
-
-var crystalGreen = Math.floor(Math.random() * 12) + 1;
-//console.log(crystalGreen);
-var green = document.getElementById("green");
-green.setAttribute("value", crystalGreen);
-
-console.log(green.value);
-
+var randomNum;
+var tar;
+var blue;
+var red;
+var yellow;
+var green;
+var crystalRed;
+var crystalBlue;
+var crystalGreen;
+var crystalYellow;
 
 // add up the value of the number that was assigned to the crystal to yourScore
 var yourScore = document.getElementById("yourScore");
-var score = 0;
+
+function setUp() {
+    randomNum = Math.floor(Math.random() * 111) + 19;
+    //console.log(randomNum);
+    tar = document.getElementById("targetScore");
+    tar.textContent = randomNum;
+
+    // assign random value between 1 - 12 for each crystal
+
+    crystalRed = Math.floor(Math.random() * 12) + 1;
+    //console.log(crystalRed);
+    red = document.getElementById("red");
+    red.setAttribute("value", crystalRed);
+    rvalue = parseInt(red.value);
+    console.log(red.value);
+
+    crystalBlue = Math.floor(Math.random() * 12) + 1;
+    //console.log(crystalBlue);
+    blue = document.getElementById("blue");
+    blue.setAttribute("value", crystalBlue);
+    bvalue = parseInt(blue.value);
+    console.log(blue.value);
+
+    crystalYellow = Math.floor(Math.random() * 12) + 1;
+    //console.log(crystalYellow);
+    yellow = document.getElementById("yellow");
+    yellow.setAttribute("value", crystalYellow);
+    yvalue = parseInt(yellow.value);
+    console.log(yellow.value);
+
+    crystalGreen = Math.floor(Math.random() * 12) + 1;
+    //console.log(crystalGreen);
+    green = document.getElementById("green");
+    green.setAttribute("value", crystalGreen);
+    gvalue = parseInt(green.value);
+    console.log(green.value);
+
+    //
+    yourScore.value = 0;
+    yourScore.textContent = 0;
+}
+setUp();
+
+
 
 function scoreRed() {
-    if (score > 0) { 
+    if (parseInt(yourScore.value) > 0) {
         yourScore.textContent = parseInt(yourScore.value) + rvalue;
         yourScore.value = parseInt(yourScore.value) + rvalue;
+        score = parseInt(yourScore.value) + rvalue;
     }
     else {
         yourScore.textContent = rvalue;
+        yourScore.value = rvalue;
         score = rvalue;
     }
     console.log(red.value);
+    
+    //check3r put here at a specific time (waiting for if else statement to run first)
+    checker();
 }
 function scoreBlue() {
-    if (yourScore.value > 0) {
+    if (parseInt(yourScore.value) > 0) {
         yourScore.textContent = parseInt(yourScore.value) + bvalue;
         yourScore.value = parseInt(yourScore.value) + bvalue;
+        score = parseInt(yourScore.value) + bvalue;
     }
     else {
         yourScore.textContent = bvalue;
-        yourScore = blue.value;
+        yourScore.value = bvalue;
+        score = bvalue;
     }
-    console.log(yourScore);
+    console.log(red.value);
+    checker();
 }
 function scoreYellow() {
-    if (yourScore > 0) {
-        yourScore.textContent = parseInt(yourScore.value) + parseInt(yellow.value);
-        yourScore.value = parseInt(yourScore.value) + parseInt(yellow.value);
+    if (parseInt(yourScore.value) > 0) {
+        yourScore.textContent = parseInt(yourScore.value) + yvalue;
+        yourScore.value = parseInt(yourScore.value) + yvalue;
+        score = parseInt(yourScore.value) + yvalue;
     }
     else {
-        yourScore.textContent = parseInt(yellow.value);
-        yourScore = yellow.value;
+        yourScore.textContent = yvalue;
+        yourScore.value = yvalue;
+        score = yvalue;
     }
-    console.log(yourScore);
+    console.log(red.value);
+    checker();
 }
 function scoreGreen() {
-    if (yourScore > 0) {
-        yourScore.textContent = parseInt(yourScore.value) + parseInt(green.value);
-        yourScore.value = parseInt(yourScore.value) + parseInt(green.value);
+    if (parseInt(yourScore.value) > 0) {
+        yourScore.textContent = parseInt(yourScore.value) + gvalue;
+        yourScore.value = parseInt(yourScore.value) + gvalue;
+        score = parseInt(yourScore.value) + gvalue;
     }
     else {
-        yourScore.textContent = parseInt(green.value);
-        yourScore = green.value;
+        yourScore.textContent = gvalue;
+        yourScore.value = gvalue;
+        score = gvalue;
     }
-    //console.log(yourScore);
+    console.log(red.value);
+    checker();
 }
 
 
-// if yourScore is greater than targetScore increment losses and restart game
+// if yourScore is greater than targetScore increment losses and restart game, function used to ensure that after every click this statement is always checked
+var wins = 0;
+var losses = 0;
+function checker() {
+    if (parseInt(yourScore.value) === parseInt(tar.textContent)) {
+        alert('You won');
+        wins++;
+        document.getElementById("wins").textContent = wins;
+        setUp();
+    }
+    else if (parseInt(yourScore.value) > parseInt(tar.textContent)) {
+        alert('You Lost');
+        losses++;
+        document.getElementById("losses").textContent = losses;
+        setUp();
+    }
+}
 
 
 // if yourScore is equall to targetScore increment wins and restart game
